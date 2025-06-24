51bf1674f9936936dfa477a6eb2c07ab9483d9c1
Aitor García Rey <aitor@linkingpaths.com>
2025-06-24 19:44:24 +0200

    feat(mcp): Add error control a params typing

--- server.rb
+++ server.rb
@@ -32,11 +32,49 @@ class EchoTool < MCP::Tool
   end
 end
 
+class DiceTool < MCP::Tool
+  VALID_DICE = [4, 6, 8, 10, 12, 20, 100].freeze
+  description 'A tool that rolls a virtual dice of the type provided and returns the result'
+  input_schema(
+    properties: {
+      dice: {
+        type: 'number'
+      }
+    },
+    required: ['dice']
+  )
+
+  class << self
+    def call(dice:)
+      return unvalid_dice_error(dice) unless VALID_DICE.include?(dice)
+
+      MCP::Tool::Response.new(
+        [
+          {
+            type: 'text',
+            text: "The d#{dice} roll is #{rand(dice) + 1}"
+          }
+        ]
+      )
+    end
+
+    def unvalid_dice_error(dice)
+      MCP::Tool::Response.new(
+        [
+          {
+            type: 'text',
+            text: "A d#{dice} is not a valid dice"
+          }
+        ], true
+      )
+    end
+  end
+end
 
 server = MCP::Server.new(
   name: 'mcp-stdio-sdk',
-  version: '1.0.4',
-  tools: [EchoTool],
+  version: '1.0.5',
+  tools: [EchoTool, DiceTool],
   prompts: [],
   resources: [],
   capabilities: { tools: { listChanged: true } }
