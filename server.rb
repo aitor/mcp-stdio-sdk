f84698fa27f63f80607bc7c8403c4cac30342b5e
Aitor García Rey <aitor@linkingpaths.com>
2025-06-24 17:53:06 +0200

    feat(mcp): Add a block tool

--- server.rb
+++ server.rb
@@ -9,10 +9,27 @@ end
 
 server = MCP::Server.new(
   name: 'mcp-stdio-sdk',
-  version: '1.0.2',
+  version: '1.0.3',
   tools: [],
   prompts: [],
-  resources: []
+  resources: [],
+  capabilities: { tools: { listChanged: true } }
 )
+
+server.define_tool(
+  name: 'echo',
+  description: 'A simple example tool that echoes back its arguments',
+  input_schema: { properties: { message: { type: 'string' } }, required: ['message'] }
+) do |message:|
+  MCP::Tool::Response.new(
+    [
+      {
+        type: 'text',
+        text: "Hello from echo tool! Message: #{message}"
+      }
+    ]
+  )
+end
+
 transport = MCP::Server::Transports::StdioTransport.new(server)
 transport.open
